5184aaa4b86084924a00fbe29a95427c66533899
Hữu Tôn <nghuuton@gmail.com>
2020-06-28 00:38:51 +0700

    update && delete Deck

--- controllers/decks.js
+++ controllers/decks.js
@@ -7,9 +7,8 @@ const index = async (req, res, next) => {
 };
 
 const newDeck = async (req, res, next) => {
-  const owner = await User.findById(req.body.owner);
-  console.log(owner);
-  const deck = req.body;
+  const owner = await User.findById(req.value.body.owner);
+  const deck = req.value.body;
   delete deck.owner;
   deck.owner = owner._id;
   const newDeck = new Deck(deck);
@@ -20,13 +19,43 @@ const newDeck = async (req, res, next) => {
 };
 
 const getDeck = async (req, res, next) => {
-  const { deckId } = req.params;
+  const { deckId } = req.value.params;
   const deck = await Deck.findById(deckId);
   return res.status(200).json({ deck });
+};
+
+const updateDeck = async (req, res, next) => {
+  const { deckId } = req.value.params;
+  const { owner } = req.value.body;
+  const newDeck = req.value.body;
+  const result = await Deck.findByIdAndUpdate(deckId, newDeck);
+  console.log("Gía trị body", newDeck, "Từ body", owner, "Khi update", result);
+  if (owner) {
+    const currentUser = await User.findById(result.owner);
+    currentUser.decks.pull(result);
+    const newUser = await User.findById(owner);
+    newUser.decks.push(result._id);
+    await currentUser.save();
+    await newUser.save();
+    console.log(currentUser.lastname, newUser.lastname);
+  }
+  return res.status(200).json({ success: true });
+};
+
+const deleteDeck = async (req, res, next) => {
+  const { deckId } = req.value.params;
+  const deck = await Deck.findById(deckId);
+  const user = await User.findById(deck.owner);
+  await deck.remove();
+  user.decks.pull(deck);
+  await user.save();
+  return res.status(200).json({ success: true });
 };
 
 module.exports = {
   index,
   newDeck,
   getDeck,
+  updateDeck,
+  deleteDeck,
 };
